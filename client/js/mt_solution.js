debff4a093b8fcf970d03488c6a18b7280bd9814
Particle 4 Dev <particle4dev@gmail.com>
2013-06-01 21:48:24 +0700

    first commit version stable v0.1

---
+++ client/js/mt_solution.js
@@ -0,0 +1,112 @@
+if (Meteor.isClient) {
+	MessagesCollection = new Meteor.Collection("messages");
+	MMessagesCollection = new Meteor.Collection("more_message");
+	CountMessagesCollection = new Meteor.Collection("count_message");
+	Session.setDefault('pagePost', 1);
+	Deps.autorun(function () {
+		Meteor.subscribe("page", [Session.get('pagePost')], function () {
+			console.log("post subscription complete!");
+			console.log(MessagesCollection.find({}));
+		});
+	});
+	
+	Session.setDefault('morePost', 1);
+	Deps.autorun(function () {
+		Meteor.subscribe("more", [Session.get('morePost')], function () {
+			console.log("more subscription complete!");
+			console.log(MMessagesCollection.find({}));
+		});
+	});
+	
+	Meteor.subscribe("count", [],function () {
+		console.log("count subscription complete!");
+		console.log(CountMessagesCollection.findOne({}));
+	});	
+
+	Template.page.helpers({
+		messages: function () {
+			return MessagesCollection.find({}, {sort: { date: -1}}).fetch();
+		},
+		count: function () {
+			var c = CountMessagesCollection.findOne({}), html = '';
+			if(c){
+				var page = Math.ceil(c.count/3)+1, currentPage = parseInt(Session.get('pagePost')), hPage = bPage = fPage = '', pageStart = parseInt(currentPage)-2, pageEnd = parseInt(currentPage)+2;
+console.log(pageEnd+"|"+currentPage);
+				if(pageStart <= 1){
+					pageStart = 1;
+				}
+				else{
+					hPage = '<a data-href="1" class="page-content"><span class="button-content">1</span></a><b>←</b>';
+				}
+				if(pageEnd>=page){
+					pageEnd = page;
+				}
+				else {
+					fPage = '<b>→</b>'+'<a data-href="'+(page-1)+'" class="page-content"><span class="button-content">'+(page-1)+'</span></a>';
+				}				
+				for(var i = pageStart; i < pageEnd; i++){
+					bPage += '<a data-href="'+i+'" class="page-content"><span class="button-content">'+i+'</span></a>';
+				}
+				
+				html = hPage + bPage + fPage ;
+				return new Handlebars.SafeString(html);
+			}				
+			return 0;
+		},
+		page_nav: function(){
+			var c = CountMessagesCollection.findOne({});
+			if(c){
+				var page = Math.ceil(c.count/3);
+				return 'Page '+Session.get('pagePost')+"/"+page;
+			}
+			return 'None';
+		}
+	});
+	Template.more.helpers({
+		messages: function () {
+			return MMessagesCollection.find({}, {sort: { date: -1}}).fetch();
+		}
+	});
+	Template.addTemplate.events({
+		'keypress': function(e, t){			
+			if(e.charCode == 13){
+				e.preventDefault();
+				Meteor.call('newMessage', t.find('input').value);
+				t.find('input').value = '';
+			}			
+		}
+	});
+	Template.page.events({
+		'click .page-content': function(e, t){
+			Session.set('pagePost', $(e.currentTarget).attr('data-href'));
+			e.preventDefault();
+		},
+		'click i.icon-remove': function(e, t){			
+			Meteor.call('removeMessage', this._id);
+			e.preventDefault();
+		},
+	});
+	Template.more.events({
+		'click .more': function(e, t){
+			var g = Session.get('morePost');
+			g++;
+			var c = CountMessagesCollection.findOne({});
+			var page = Math.ceil(c.count/3);
+			if(g > page) g = page;
+			Session.set('morePost', g);
+			e.preventDefault();
+		},
+		'click .less': function(e, t){
+			var g = Session.get('morePost');
+			g--;
+			if(g<=0) g = 1;
+			Session.set('morePost', g);
+			e.preventDefault();
+		},
+		'click i.icon-remove': function(e, t){			
+			Meteor.call('removeMessage', this._id);
+			e.preventDefault();
+		},
+	});
+	
+}
---
+++ server/mt_solution.js
@@ -0,0 +1,77 @@
+if (Meteor.isServer) {
+	var perPage = 3;
+	MessagesCollection = new Meteor.Collection("messages");
+	Meteor.methods({
+		newMessage: function(newM){
+			MessagesCollection.insert({message: newM, date: Date.now()});
+		},
+		removeMessage: function(id){
+			MessagesCollection.remove({_id: id});
+		}
+	});	
+	Meteor.publish("page", function(page){
+		page -= 1;
+		if(page < 0) page = 0;
+		var start = page * perPage;
+		return MessagesCollection.find({}, {sort: { date: -1}, skip: start, limit: perPage, fields: {}});
+	});
+	Meteor.publish("more", function (page) {
+		page -= 1;
+		if(page < 0) page = 0;
+		var start = page * perPage + perPage, isVisibleCursor, observerHandle, self = this;
+		isVisibleCursor = MessagesCollection.find({}, {sort: { date: -1}, limit: start, fields: {}});
+		
+		_.each(isVisibleCursor.fetch(), function(e, i){
+			self.added("more_message", e._id, e);
+		}, self);		
+		observerHandle = isVisibleCursor.observeChanges({
+			added: function (idx, doc) {				
+				console.log('MessagesCollection added '+ idx);
+				if(idx){
+					self.added("more_message", idx, doc);
+					self.ready();
+				}								
+			},
+			removed: function (idx) {
+				console.log('MessagesCollection removed '+ idx);
+				if(idx){
+					self.removed("more_message", idx);
+					self.ready();
+				}					
+			}
+		});		
+		self.ready();
+		self.onStop(function () {
+			observerHandle.stop();
+		});
+	});
+	Meteor.publish("count", function () {		
+		var docId = Meteor.uuid(), isVisibleCursor = MessagesCollection.find({}, {sort: { date: -1}}), count = 0, self = this;
+		self.added("count_message", docId, {count: count});				
+		observerHandle = isVisibleCursor.observeChanges({
+			added: function (idx, doc) {				
+				if(idx){
+					count ++;
+					console.log('MessagesCollection count added '+ count);
+					self.changed("count_message", docId, {count: count});
+					self.ready();
+				}				
+			},
+			removed: function (idx) {				
+				if(idx){
+					count--;
+					console.log('MessagesCollection count removed '+ count);
+					self.changed("count_message", docId, {count: count});
+					self.ready();
+				}
+			}
+		});		
+		self.ready();
+		self.onStop(function () {
+			observerHandle.stop();
+		});
+	});
+	Meteor.startup(function () {
+	// code to run on server at startup
+	});
+}
